c8e6ce56065b1b67360d625caf7fe26fc1a9fac2
deniztraka <deniz.traka@gmail.com>
2020-06-29 01:04:47 +0300

    player animations

--- Assets/DTWorldz/Scripts/Behaviours/PlayerMovementBehaviour.cs
+++ Assets/DTWorldz/Scripts/Behaviours/PlayerMovementBehaviour.cs
@@ -6,26 +6,64 @@ namespace DTWorldz.Behaviours
     [RequireComponent(typeof(Rigidbody2D))]
     public class PlayerMovementBehaviour : MonoBehaviour
     {
-        public float Speed = 1f;   //Movement Speed of the Player
+        public float Speed = 3f;   //Movement Speed of the Player
+        public float RunningSpeed = 5f;   //Movement Speed of the Player
+        private float resultingSpeed = 1f;   //Movement Speed of the Player
+        private bool isRunning = false;
         private Vector2 movement;           //Movement Axis
         private Rigidbody2D rigidbody2d;      //Player Rigidbody Component
+        private Animator animator;           //animator
 
         // Start is called before the first frame update
         void Start()
         {
             rigidbody2d = this.GetComponent<Rigidbody2D>();
+            animator = this.GetComponent<Animator>();
+
         }
 
         // Update is called once per frame
         void Update()
         {
+            HandleInput();
+            HandleAnimations();
+        }
+
+        private void HandleInput()
+        {
             movement.x = Input.GetAxisRaw("Horizontal");
             movement.y = Input.GetAxisRaw("Vertical");
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                isRunning = false;
+                Debug.Log("keyuop");
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                Debug.Log("keydown");
+                isRunning = true;
+            }
+        }
+
+        private void HandleAnimations()
+        {
+            if (movement != Vector2.zero)
+            {
+                animator.SetFloat("Horizontal", movement.x);
+                animator.SetFloat("Vertical", movement.y);
+                resultingSpeed = isRunning ? RunningSpeed : Speed;
+            }
+            else
+            {
+                resultingSpeed = 0;
+            }
+            animator.SetFloat("MovementSpeed", resultingSpeed);
         }
 
         private void FixedUpdate()
         {
-            rigidbody2d.MovePosition(rigidbody2d.position + movement * Speed * Time.fixedDeltaTime);
+            rigidbody2d.MovePosition(rigidbody2d.position + movement * resultingSpeed * Time.fixedDeltaTime);
         }
     }
 }
