ad1e0df8c49939eff1ea9f0f5f2e2426705fc50a
Deniz Traka <d.traka@tr.emakina.com>
2020-08-24 14:09:30 +0300

    Spawner

---
+++ Assets/DTWorldz/Scripts/Editor/Gizmo/SpawnerGizmo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SpawnerGizmo : MonoBehaviour
+{
+    [DrawGizmo(GizmoType.Selected | GizmoType.NotInSelectionHierarchy)]
+    static void DrawStateGizmo(ObjectSpawnerBehaviour spawner, GizmoType gizmoType)
+    {
+
+        Gizmos.color = Color.red;
+        var positionY = spawner.transform.position.y;
+        var positionX = spawner.transform.position.x;
+        var gizmoPosition = new Vector3(positionX, positionY, 0);
+        Handles.Label(gizmoPosition, "spawner");
+
+        var rangeColor = Color.red;
+        rangeColor.a = 0.1f;
+        Gizmos.color = spawner.Color;
+        Gizmos.DrawCube(spawner.transform.position, new Vector3(spawner.RangeX,spawner.RangeY,0));        
+    }
+}
