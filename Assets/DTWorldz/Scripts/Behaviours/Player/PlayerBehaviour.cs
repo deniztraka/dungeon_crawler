c10bddedda97e6528e93670f8de599f3717135b2
deniztraka <deniz.traka@gmail.com>
2020-10-09 18:51:21 +0300

    action button cooldown animation is added

--- Assets/DTWorldz/Scripts/Behaviours/Player/PlayerBehaviour.cs
+++ Assets/DTWorldz/Scripts/Behaviours/Player/PlayerBehaviour.cs
@@ -2,14 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using DTWorldz.Behaviours.Mobiles;
+using DTWorldz.Behaviours.UI;
 using UnityEngine;
 namespace DTWorldz.Behaviours.Player
 {
     public class PlayerBehaviour : MonoBehaviour
     {
+        public ActionButtonBehaviour ActionButtonBehaviour;
         // Start is called before the first frame update
         void Start()
         {
+            var movementBehaviour = GetComponent<PlayerMovementBehaviour>();
+            var attackBehaviour = GetComponentInChildren<AttackBehaviour>();
+            ActionButtonBehaviour.SetAction(movementBehaviour.Attack, attackBehaviour.AttackingFrequency);
 
         }
 
---
+++ Assets/DTWorldz/Scripts/Behaviours/UI/ActionButtonBehaviour.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace DTWorldz.Behaviours.UI
+{
+    public class ActionButtonBehaviour : MonoBehaviour
+    {
+        private bool isCooldown = false;
+        private float cooldownTime = 0f;
+
+        public UnityEvent ActionEvent;
+        public Button Button;
+        public Animator Animator;
+        public Image FillImage;
+        public Image ActionImage;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (Button == null)
+            {
+                Button = gameObject.GetComponent<Button>();
+            }
+            if (FillImage == null)
+            {
+                FillImage = gameObject.GetComponentInChildren<Image>();
+            }
+            if (Animator == null)
+            {
+                Animator = gameObject.GetComponent<Animator>();
+            }
+
+            Button.onClick.AddListener(Action);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!isCooldown)
+            {
+                Button.enabled = true;
+                FillImage.fillAmount = 1;
+            }
+            else
+            {
+
+                FillImage.fillAmount -= 1 / cooldownTime * Time.deltaTime;
+                if (FillImage.fillAmount <= 0)
+                {
+                    FillImage.fillAmount = 0;
+                    isCooldown = false;
+                }
+            }
+        }
+
+        public void Action()
+        {
+            if (ActionEvent != null)
+            {
+                Animator.Play("OnClick");
+                isCooldown = true;
+                Button.enabled = false;
+                ActionEvent.Invoke();
+            }
+        }
+
+        public void SetAction(UnityAction action, float cooldown)
+        {
+            ActionEvent.RemoveAllListeners();
+            ActionEvent.AddListener(action);
+            cooldownTime = cooldown;
+        }
+
+        public void SetAction(UnityAction action, float cooldown, Sprite actionImage)
+        {
+           SetAction(action,cooldown);
+           ActionImage.sprite = actionImage;
+
+        }
+
+        public void SetCoolDown(float cooldown)
+        {
+            cooldownTime = cooldown;
+        }
+
+        public void SetActionImage(Sprite actionImage)
+        {
+            ActionImage.sprite = actionImage;
+        }
+    }
+}
