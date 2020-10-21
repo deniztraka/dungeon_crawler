f4c81c18062d50262d5e44c6b640cc6c912e9cf6
deniztraka <deniz.traka@gmail.com>
2020-10-22 02:14:11 +0300

    trees

---
+++ Assets/DTWorldz/Scripts/Behaviours/Utils/HideIfBehindBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace DTWorldz.Behaviours.Utils
+{
+    public class HideIfBehindBehaviour : MonoBehaviour
+    {
+        public float OnOverOpacity = 0.75f;
+        public SpriteRenderer RendererToHide;
+        private GameObject player;
+        private Color initialColor;
+        // Start is called before the first frame update
+        void Start()
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            if (RendererToHide)
+            {
+                initialColor = RendererToHide.color;
+            }
+        }
+
+        public void OnTriggerEnter2D(Collider2D collider)
+        {
+            if (collider.tag == "Player")
+            {
+                if (RendererToHide)
+                {
+                    var newColor = new Color(initialColor.r, initialColor.g, initialColor.b, OnOverOpacity);
+                    RendererToHide.color = newColor;
+                }
+            }
+        }
+
+        public void OnTriggerExit2D(Collider2D collider)
+        {
+            if (collider.tag == "Player")
+            {
+                if (RendererToHide)
+                {
+                    RendererToHide.color = initialColor;
+                }
+            }
+        }
+    }
+}
