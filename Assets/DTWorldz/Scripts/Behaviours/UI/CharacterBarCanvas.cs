15c8e77f5d6cf8f741ebadaec0c06d91e206a386
Deniz Traka <deniz.traka@gmail.com>
2020-11-09 20:40:49 +0300

    character panel UI

--- Assets/DTWorldz/Scripts/Behaviours/UI/CharacterBarCanvas.cs
+++ Assets/DTWorldz/Scripts/Behaviours/UI/CharacterBarCanvas.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 using UnityEngine.UI;
 namespace DTWorldz.Behaviours.UI
 {
+    
     public class CharacterBarCanvas : MonoBehaviour
     {
+        public Canvas CharacterPanelCanvas;
         public Text CharacterTitleText;
         public void UpdateCharacterTitleText(string text)
         {
             CharacterTitleText.text = text;
+        }
+
+        public void OpenCharacterPanel(){
+            CharacterPanelCanvas.enabled = true;
         }
     }
 }
---
+++ Assets/DTWorldz/Scripts/Behaviours/UI/CharacterPanelCanvasBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace DTWorldz.Behaviours.UI
+{
+    public class CharacterPanelCanvasBehaviour : MonoBehaviour
+    {
+        public void ClosePanel(){
+            var canvas = GetComponent<Canvas>();
+            canvas.enabled = false;
+        }
+    }
+}
