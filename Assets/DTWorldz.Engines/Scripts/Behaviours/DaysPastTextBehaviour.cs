7f0f0a77e843dbd070fa312501ac6446d25d3808
Deniz Traka <deniz.traka@gmail.com>
2021-04-30 23:39:30 +0300

    time of the day

---
+++ Assets/DTWorldz.Engines/Scripts/Behaviours/DaysPastTextBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DTEngines.Behaviours
+{
+    public class DaysPastTextBehaviour : TimeOfDayUIBehaviour
+    {
+        public string Format;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (string.IsNullOrEmpty(Format))
+            {
+                Format = "{1}:{2} - {0} days";
+            }
+        }
+
+        public override void UpdateMe()
+        {
+            if (TimeOfTheDay != null)
+            {
+                var currentGameTime = TimeOfTheDay.GetGameTime();
+                var textComp = GetComponent<Text>();
+                textComp.text = string.Format(Format,
+                currentGameTime.Days,
+                currentGameTime.Hours.ToString().Length == 1 ? ("0" + currentGameTime.Hours.ToString()) : currentGameTime.Hours.ToString(),
+                currentGameTime.Minutes.ToString().Length == 1 ? ("0" + currentGameTime.Minutes.ToString()) : currentGameTime.Minutes.ToString(),
+                currentGameTime.Seconds);
+            }
+        }
+    }
+}
---
+++ Assets/DTWorldz.Engines/Scripts/Behaviours/TimeOfTheDayAmbientBehaviour.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+namespace DTEngines.Behaviours
+{
+    public class TimeOfTheDayAmbientBehaviour : TimeOfDayUIBehaviour
+    {
+        public Color[] AmbientHourColorsList;
+
+        //private SpriteRenderer ambientSpriteRenderer;
+        [SerializeField]
+        private Light2D ambientLight;
+        private Color targetColor;
+
+        private float ratio;
+        private float tweenLength;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // ambientSpriteRenderer = GetComponent<SpriteRenderer>();
+            // targetColor = ambientSpriteRenderer.color;
+            ambientLight = GetComponent<Light2D>();
+            targetColor = ambientLight.color;
+            ratio = TimeOfTheDay.DayLengthInSeconds / 86400f;
+
+            if (TimeOfTheDay != null)
+            {
+                targetColor = AmbientHourColorsList[TimeOfTheDay.GetGameTime().Hours];
+            }
+        }
+        public override void UpdateMe()
+        {
+            tweenLength = 0f;
+            if (TimeOfTheDay != null)
+            {
+                var currentGameTime = TimeOfTheDay.GetGameTime();
+                targetColor = AmbientHourColorsList[currentGameTime.Hours];
+                // if (currentGameTime.Hours >= 6 && currentGameTime.Hours <= 18)
+                // {
+                //     targetColor = DayColor;
+                //     tweenLength = ratio * 10 * 60;
+                // }
+                // else
+                // {
+                //     targetColor = NightColor;
+                //     tweenLength = ratio * 14 * 60;
+                // }
+
+                // if (currentGameTime.Hours >= 0 && currentGameTime.Hours <= 4)
+                // {
+                //     targetColor = NightColor;
+                //     tweenLength  = ratio * (4/24);
+                // }
+                // else 
+                // if (currentGameTime.Hours > 4 && currentGameTime.Hours <= 9)
+                // {
+                //     targetColor = MorningColor;
+                //     tweenLength  = ratio * (5/24);
+                // }
+                // else if (currentGameTime.Hours > 9 && currentGameTime.Hours <= 15)
+                // {
+                //     targetColor = DayColor;
+                //     tweenLength  = ratio * (6/24);
+                // }
+                // else if (currentGameTime.Hours > 15 && currentGameTime.Hours <= 18)
+                // {
+                //     targetColor = DuskColor;
+                //     tweenLength  = ratio * (3/24);
+                // }
+                // else if ( currentGameTime.Hours > 18 && currentGameTime.Hours <= 21)
+                // {
+                //     targetColor = NightColor;
+                //     tweenLength  = ratio * (3/24);
+                // }
+
+            }
+        }
+
+        void Update()
+        {
+            //ambientSpriteRenderer.color = Color.Lerp(ambientSpriteRenderer.color, targetColor, /*tweenLength / */Time.deltaTime);
+            if(ambientLight == null){
+                return;
+            }
+            ambientLight.color = Color.Lerp(ambientLight.color, targetColor, /*tweenLength / */Time.deltaTime);
+            //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, targetAngle), Time.deltaTime);
+        }
+    }
+}
